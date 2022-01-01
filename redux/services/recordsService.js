cf2dc0387b1c5a0d77103ce3bd628d70451458e0
Istvan Polacsek <stepan.polachek@gmail.com>
2022-01-01 19:09:00 +0100

    devnew recordsService.js refactor

--- redux/services/recordsService.js
+++ redux/services/recordsService.js
@@ -1,84 +1,110 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
 import { assign, find, map, without } from 'lodash';
 
+import { selectFilter, selectLimit } from 'redux/slices/records';
 import { DELETE, POST, PUT } from 'lib/constants';
-import { selectFilter } from 'redux/slices/records';
 
+const CREATE_RECORD = 'createRecord';
+const RETRIEVE_RECORDS = 'retrieveRecords';
+const UPDATE_RECORD = 'updateRecord';
+const DELETE_RECORD = 'deleteRecord';
 const type = 'record';
 
-export const recordsApi = createApi({
-  reducerPath: 'recordsAPI',
-  baseQuery: fetchBaseQuery({ baseUrl: 'api/' }),
-  tagTypes: [type],
+const baseQuery = fetchBaseQuery({ baseUrl: 'api/' });
+
+export const recordsApi = createApi({ reducerPath: 'recordsAPI', baseQuery, tagTypes: [type] });
+
+const { util } = recordsApi;
+
+const onCreateQueryStarted = async(body, { dispatch, queryFulfilled, getState }) => {
+  try {
+    const { filter } = selectFilter(getState());
+    const { limit } = selectLimit(getState());
+
+    const { updateQueryData } = util;
+
+    const patch = dispatch(updateQueryData(
+      RETRIEVE_RECORDS,
+      { filter, limit },
+      (draft) => ([body, ...draft]).sort((a, b) => new Date(b.traveldate) - new Date(a.traveldate)),
+    ));
+
+    try {
+      await queryFulfilled;
+    } catch {
+      patch.undo();
+    }
+  } catch (e) {
+
+  }
+};
+
+const onUpdateQueryStarted = async(body, { dispatch, queryFulfilled, getState }) => {
+  try {
+    const { filter } = selectFilter(getState());
+    const { limit } = selectLimit(getState());
+    const { _id } = body;
+    const { updateQueryData } = util;
+
+    const patch = updateQueryData(
+      RETRIEVE_RECORDS,
+      { filter, limit },
+      (draft) => {
+        const updated = find(draft, { _id });
+
+        assign(updated, body);
+      },
+    );
+
+    try {
+      await queryFulfilled;
+    } catch {
+      patch.undo();
+    }
+  } catch (e) {
+
+  }
+};
+
+const onDeleteQueryStarted = async(_id, { dispatch, queryFulfilled, getState }) => {
+  try {
+    const { filter } = selectFilter(getState());
+    const { limit } = selectLimit(getState());
+    const { updateQueryData } = util;
+
+    const patch = updateQueryData(
+      RETRIEVE_RECORDS,
+      { filter, limit },
+      (draft) => without(draft, { _id }),
+    );
+  } catch (e) {
+
+  }
+};
+
+recordsApi.injectEndpoints({
   endpoints: ({ query, mutation }) => ({
-    createRecord: mutation({
+    [CREATE_RECORD]: mutation({
       query: (body) => ({ url: '/records', method: POST, body }),
       invalidatesTags: [{ type, id: 'LIST' }],
-      onQueryStarted: async(body, { dispatch, queryFulfilled, getState }) => {
-        const { util } = recordsApi;
-        const filter = selectFilter(getState());
-        const patch = dispatch(util.updateQueryData(
-          'retrieveRecords',
-          { filter },
-          (draft) =>
-            ([body, ...draft]).sort((a, b) => new Date(b.traveldate) - new Date(a.traveldate)),
-        ));
-
-        try {
-          await queryFulfilled;
-        } catch {
-          patch.undo();
-        }
-      },
+      onQueryStarted: onCreateQueryStarted,
     }),
-    retrieveRecords: query({
+    [RETRIEVE_RECORDS]: query({
       query: ({ limit = 10, filter }) => ({ url: '/records', params: { limit, filter } }),
       providesTags: (results) => [
         ...(results ? [...map(results, ({ _id: id }) => ({ type, id }))] : []),
         { type, id: 'LIST' },
       ],
     }),
-    updateRecord: mutation({
+    [UPDATE_RECORD]: mutation({
       query: (body) => ({ url: `/records/${body._id}`, method: PUT, body }),
       invalidatesTags: (results, error, { _id: id }) => [{ type, id }],
-      onQueryStarted: async(body, { dispatch, queryFulfilled, getState }) => {
-        const { _id } = body;
-        const { util } = recordsApi;
-        const filter = selectFilter(getState());
-        const patch = dispatch(util.updateQueryData(
-          'retrieveRecords',
-          { filter },
-          (draft) => {
-            const updated = find(draft, { _id });
-
-            assign(updated, body);
-          }));
-
-        try {
-          await queryFulfilled;
-        } catch {
-          patch.undo();
-        }
-      },
+      onQueryStarted: onUpdateQueryStarted,
     }),
-    deleteRecord: mutation({
+    [DELETE_RECORD]: mutation({
       query: (id) => ({ url: `/records/${id}`, method: DELETE }),
       invalidatesTags: (results, error, id) => [{ type, id }],
-      onQueryStarted: async(_id, { dispatch, queryFulfilled, getState }) => {
-        const { util } = recordsApi;
-        const filter = selectFilter(getState());
-        const patch = dispatch(util.updateQueryData(
-          'retrieveRecords',
-          { filter },
-          (draft) => without(draft, { _id }),
-        ));
-
-        try {
-          await queryFulfilled;
-        } catch {
-          patch.undo();
-        }
-      },
+      onQueryStarted: onDeleteQueryStarted,
     }),
   }),
 });
