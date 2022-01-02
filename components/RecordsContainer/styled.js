a6ed6927b1821dca21a52620d8eb0e578c1dfd72
Istvan Polacsek <stepan.polachek@gmail.com>
2022-01-02 19:35:00 +0100

    devnew ios tweaks

--- components/RecordsContainer/styled.js
+++ components/RecordsContainer/styled.js
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled';
 import { Grid } from '@mui/material';
 
+const margin = 10;
+
 export const LoaderItemStyled = styled(Grid)`
   height: 60px;
-  padding-top: 10px;
-  padding-bottom: calc(${({ theme }) => theme.mixins.toolbar.minHeight}px + env(safe-area-inset-bottom, 0));
+  margin-top: ${margin}px;
+  padding-bottom: calc(${({ theme }) => theme.mixins.toolbar.minHeight}px + env(safe-area-inset-bottom, 0) + ${margin}px);
 `;
