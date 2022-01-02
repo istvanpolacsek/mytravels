dec925e123a4fe14686a5a89a744f59897ff04b0
Istvan Polacsek <stepan.polachek@gmail.com>
2022-01-02 20:11:58 +0100

    devnew styles fix

--- components/TravelTypeFilter/styled.js
+++ components/TravelTypeFilter/styled.js
@@ -1,11 +1,11 @@
 import styled from '@emotion/styled';
 import { AppBar, Toolbar } from '@mui/material';
 
-export const CenteredToolbarStyled = styled(Toolbar)`
-  justify-content: center;
+export const TravelTypeAppbarStyled = styled(AppBar)`
+  z-index: ${({ theme }) => theme.zIndex.speedDial};
 `;
 
-export const TravelTypeFilterStyled = styled(AppBar)`
+export const TravelTypeToolbarStyled = styled(Toolbar)`
+  justify-content: center;
   margin-top: calc(${({ theme }) => theme.mixins.toolbar.minHeight}px + env(safe-area-inset-top, 0));
-  z-index: ${({ theme }) => theme.zIndex.speedDial}px;
 `;
