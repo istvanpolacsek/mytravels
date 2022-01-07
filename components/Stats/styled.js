3467214aecb6ecdc52ffedb33907ec86ee9eba77
Istvan Polacsek <stepan.polachek@gmail.com>
2022-01-07 14:40:22 +0100

    dev-stats bugfixes

---
+++ components/Stats/styled.js
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+import { ResponsiveContainer } from 'recharts';
+
+export const StatsPieChartStyled = styled(ResponsiveContainer)`
+  touch-action: none;
+  pointer-events: none
+`;
