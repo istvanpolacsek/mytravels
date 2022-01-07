f2a311d121bad5fa95500b29b1644fd260b68a28
Istvan Polacsek <stepan.polachek@gmail.com>
2022-01-07 13:42:33 +0100

    dev-oauth stats

--- components/Navigation/ViewStatsButton.js
+++ components/Navigation/ViewStatsButton.js
@@ -2,9 +2,13 @@ import { memo } from 'react';
 import { IconButton } from '@mui/material';
 import { RiBarChartLine } from 'react-icons/ri';
 
+import useRoutes from 'hooks/useRoutes';
+
 function ViewStatsButton() {
+  const { toStatsPage } = useRoutes();
+
   return (
-    <IconButton>
+    <IconButton onClick={toStatsPage}>
       <RiBarChartLine />
     </IconButton>
   );
--- redux/store.js
+++ redux/store.js
@@ -3,12 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import reducer from 'redux/index';
 import { recordsApi } from 'redux/services/recordsService';
+import { recordStatsApi } from 'redux/services/recordStatsService';
 
 const { middleware: recordsApiMiddleware } = recordsApi;
+const { middleware: recordStatsApiMiddleware } = recordStatsApi;
 
 export default configureStore({
   reducer,
   middleware: (getDefaultMiddleware) => concat(getDefaultMiddleware({
     serializableCheck: false,
-  }), recordsApiMiddleware),
+  }), recordsApiMiddleware, recordStatsApiMiddleware),
 });
