7cd7eb4c8079c5d79699a9dc5b90b49508f3b471
Istvan Polacsek <stepan.polachek@gmail.com>
2022-01-08 10:56:51 +0100

    dev-stats

--- components/Stats/StatsPieChart.js
+++ components/Stats/StatsPieChart.js
@@ -1,9 +1,9 @@
 import { memo } from 'react';
 import { get, map } from 'lodash';
 import { Cell, Legend, Pie, PieChart } from 'recharts';
-import { Grid } from '@mui/material';
 
 import StatsPieChartLegend from 'components/Stats/StatsPieChartLegend';
+import { StatsPieChartStyled } from 'components/Stats/styled';
 
 function StatsPieChart({ data }) {
   const { kilometers, cities } = data;
@@ -12,14 +12,14 @@ function StatsPieChart({ data }) {
   const getFillColor = (i) => `hsl(182, 100%, ${20 + i * 5}%)`;
 
   return (
-    <Grid container justifyContent="center">
+    <StatsPieChartStyled container justifyContent="center">
       <PieChart width={300} height={350}>
         <Pie cx="50%" outerRadius={60} data={pieStats} dataKey="value" nameKey="name">
           {map(pieStats, (el, i) => <Cell key={`cell-${i}`} fill={getFillColor(i)} />)}
         </Pie>
         <Legend content={<StatsPieChartLegend cities={cities} kilometers={kilometers} />} />
       </PieChart>
-    </Grid>
+    </StatsPieChartStyled>
   );
 }
 
--- components/Stats/styled.js
+++ components/Stats/styled.js
@@ -1,4 +1,10 @@
 import styled from '@emotion/styled';
+import { Grid } from '@mui/material';
+
+export const StatsPieChartStyled = styled(Grid)`
+  pointer-events: none;
+  touch-action: none;
+`;
 
 export const StatsPieChartLegendStyled = styled.div`
   height: 150px;
